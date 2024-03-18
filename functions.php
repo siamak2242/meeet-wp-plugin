9bc453bc9caa78932195d01f3ca6e6b311dfc704
siamak haha <siamak2242@yahoo.com>
2024-03-18 17:56:25 +0330

    repear add menu function

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
-function _meeet_admin_menu() {
-    include __DIR__ . '/templates/admin-page.php';
+function _meeet_admin_menu()
+{
+    add_menu_page('MEEET', 'Meeet', 'manage_options', 'meeet', function () {
+        include __DIR__ . '/templates/admin-page.php';
+    });
 }
