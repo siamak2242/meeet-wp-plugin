f677f0bf090739bb71ea86cc91f3f9961a917901
siamak haha <siamak2242@yahoo.com>
2024-03-18 22:23:55 +0330

    add assets file

--- functions.php
+++ functions.php
@@ -1,8 +1,24 @@
 <?php
 
+const _test_version = '1.0.0';
+
 function _meeet_admin_menu()
 {
     add_menu_page('MEEET', 'Meeet', 'manage_options', 'meeet', function () {
         include __DIR__ . '/templates/admin-page.php';
     });
+}
+
+function _meeet_admin_scripts()
+{
+    wp_enqueue_style(
+        'meeet-admin-page-style',
+        plugins_url('/assets/admin/css/admin-menu-page.css?v=' . _test_version, __FILE__)
+    );
+
+    wp_enqueue_script(
+        'meeet-admin-page-script',
+        plugins_url('/assets/admin/css/admin-menu-page.js?v=' . _test_version, __FILE__)
+    );
+
 }
--- settings.php
+++ settings.php
@@ -3,3 +3,4 @@
 require_once __DIR__ . '/functions.php';
 
 add_action('admin_menu', '_meeet_admin_menu');
+add_action('admin_enqueue_scripts', '_meeet_admin_scripts');
