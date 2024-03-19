d316cb864fe1b9ff715f75d6fb0878254905734f
siamak haha <siamak2242@yahoo.com>
2024-03-19 23:38:45 +0330

    update elementor-weidgets tab template

--- templates/admin-tabs/elementor-widgets.php
+++ templates/admin-tabs/elementor-widgets.php
@@ -1,13 +1,13 @@
 <div class="tab-title" data-lib-tabbing-el="tab-title">ویجت&zwnj;های المنتور</div>
 
 <div class="tab-table">
-    <div class="label">لیست ویجت&zwnj;ها</div>
+    <span class="label">لیست ویجت&zwnj;ها</span>
+    <span class="descr">ویجت هایی که نیاز دارید را انتخاب کنید</span>
     <table>
         <tr>
-            <td><label for="meeet-elementor-widget-carousel">گردونه</label></td>
             <td>
-                <input id="meeet-elementor-widget-carousel" data-option-token="_meeet_elementor_widget_carousel"
-                       type="checkbox">
+                <input id="meeet-elementor-widget-carousel" data-option-token="_meeet_elementor_widget_carousel" type="checkbox">
+                <label for="meeet-elementor-widget-carousel">گردونه</label>
             </td>
         </tr>
     </table>
