9fdb2b9b5a0f225f2c3e3b6a4e900baeb8483754
siamak haha <siamak2242@yahoo.com>
2024-03-19 17:33:34 +0330

    implement checkbox

--- templates/admin-tabs/public.php
+++ templates/admin-tabs/public.php
@@ -1,11 +1,15 @@
 <div class="tab-title" data-lib-tabbing-el="tab-title">تنظیمات عمومی</div>
 
 <div class="tab-table">
-    <div class="label">تنظیمات داشبورد</div>
+    <span class="label">تنظیمات داشبورد</span>
+    <span class="descr">تنظیمات منوی نمایش داشبورد و مدیریت محتوا</span>
     <table>
         <tr>
             <td><label for="meeet-wpcontent-margin">فاصله کناری</label></td>
-            <td><input id="meeet-wpcontent-margin" data-option-token="_meeet_wpcontent_margin" type="checkbox"></td>
+            <td>
+                <input id="meeet-wpcontent-margin" data-option-token="_meeet_wpcontent_margin" type="checkbox">
+                <label for="meeet-wpcontent-margin"></label>
+            </td>
         </tr>
     </table>
 </div>
