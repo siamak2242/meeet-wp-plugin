fb54ba9cb7813c10f988016d8d1ac60661e2e204
siamak haha <siamak2242@yahoo.com>
2024-03-19 13:38:32 +0330

    implement tabs content

--- templates/admin-tabs/elementor-settings.php
+++ templates/admin-tabs/elementor-settings.php
@@ -1,1 +1,11 @@
 <div class="tab-title" data-lib-tabbing-el="tab-title">تنظیمات المنتور</div>
+
+<div class="tab-table">
+    <div class="label">تنظیمات دسته بندی ویجت&zwnj;های المنتور</div>
+    <table>
+        <tr>
+            <td><label for="meeet-elementor-widgets-category-name">نام دسته بندی</label></td>
+            <td><input id="meeet-elementor-widgets-category-name" type="text" data-option-token="_meeet_elementor_category_name"></td>
+        </tr>
+    </table>
+</div>
--- templates/admin-tabs/public.php
+++ templates/admin-tabs/public.php
@@ -1,1 +1,11 @@
 <div class="tab-title" data-lib-tabbing-el="tab-title">عمومی</div>
+
+<div class="tab-table">
+    <div class="label">تنظیمات داشبورد</div>
+    <table>
+        <tr>
+            <td><label for="meeet-wpcontent-margin">فاصله کناری</label></td>
+            <td><input id="meeet-wpcontent-margin" data-option-token="_meeet_wpcontent_margin" type="checkbox"></td>
+        </tr>
+    </table>
+</div>
