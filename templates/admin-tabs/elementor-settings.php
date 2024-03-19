c8f993c87131a6ca4732c12e84693141286cdfff
siamak haha <siamak2242@yahoo.com>
2024-03-19 23:34:59 +0330

    implement text fields style

--- templates/admin-tabs/elementor-settings.php
+++ templates/admin-tabs/elementor-settings.php
@@ -1,7 +1,8 @@
 <div class="tab-title" data-lib-tabbing-el="tab-title">تنظیمات المنتور</div>
 
 <div class="tab-table">
-    <div class="label">تنظیمات دسته بندی ویجت&zwnj;های المنتور</div>
+    <span class="label">تنظیمات دسته بندی ویجت&zwnj;های المنتور</span>
+    <span class="descr">ویژگی های دسته بندی ویجت&zwnj;های المنتور</span>
     <table>
         <tr>
             <td><label for="meeet-elementor-widgets-category-name">نام دسته بندی</label></td>
